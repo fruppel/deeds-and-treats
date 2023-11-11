628a2f6a7f77132ecb059112df85d4b7b3b52ad0
Florian Ruppel <florian.ruppel@check24.de>
2023-11-11 16:41:37 +0100

    Fix progressbar display

--- resources/js/components/ProgressBar.vue
+++ resources/js/components/ProgressBar.vue
@@ -3,7 +3,7 @@
         <div class="mt-1 w-full bg-gray-200 dark:bg-gray-900 rounded-full h-6 text-center">
             <div
                 class="bg-teal-400 dark:bg-teal-700 h-6 rounded-full"
-                :style="{ width: formatPercent(percent) }"
+                :style="{ width: progressbarWidth(percent) }"
             ></div>
         </div>
         <div class="w-full h-6 flex items-center justify-center absolute top-0 left-0 text-xs text-center text-black dark:text-gray-200 font-bold">

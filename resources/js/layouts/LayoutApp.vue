f93daf6ffe31f6b5fcb7a5e628e97ac46569aa55
Florian Ruppel <ruppel.florian@gmail.com>
2025-04-23 15:22:39 +0200

    Make header sticky

--- resources/js/layouts/LayoutApp.vue
+++ resources/js/layouts/LayoutApp.vue
@@ -1,9 +1,9 @@
 <template>
     <div class="bg-gray-100 dark:bg-gray-900 min-h-screen">
-        <nav class="relative text-black dark:text-gray-200 bg-white dark:bg-gray-800 border-b border-gray-100 dark:border-gray-900 z-10  mb-2">
+        <nav class="sticky top-0 text-black dark:text-gray-200 bg-white dark:bg-gray-800 border-b border-gray-100 dark:border-gray-900 z-10  mb-2">
             <div class="flex justify-between px-4 h-12">
                 <div class="flex items-center font-semibold text-lg">
-                    {{ title }}
+                    {{ title }} asd
                 </div>
                 <mobile-menu :routes="{}"></mobile-menu>
                 <app-drawer></app-drawer>

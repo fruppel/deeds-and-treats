e90f36a8fbdfa9efabd144acec5e8f1870d96847
Florian Ruppel <ruppel.florian@gmail.com>
2025-09-03 18:11:34 +0200

    Fix setting

--- resources/js/layouts/LayoutApp.vue
+++ resources/js/layouts/LayoutApp.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="bg-gray-100 dark:bg-gray-900 min-h-screen" data-theme="dark">
+    <div class="bg-gray-100 dark:bg-gray-900 min-h-screen" :data-theme="theme">
         <nav class="sticky top-0 text-black dark:text-gray-200 bg-white dark:bg-gray-800 border-b border-gray-100 dark:border-gray-900 z-10 mb-2">
             <div class="flex justify-between px-4 h-12">
                 <div class="flex items-center font-semibold text-lg">
@@ -18,7 +18,7 @@
 </template>
 
 <script setup>
-import {computed} from 'vue';
+import {computed, ref} from 'vue';
 import { RouterView } from 'vue-router';
 import router from '@/router';
 import MobileMenu from '../components/layout/MobileMenu.vue';
@@ -26,8 +26,11 @@ import AppDrawer from '../components/AppDrawer.vue';
 import ToastContainer from '@/components/ToastContainer.vue';
 import useUserStore from '@/stores/user.js';
 
-
 const title = computed(() => router.currentRoute.value.meta?.title || 'Deeds & Treats');
+let theme = ref('light');
+
 const userStore = useUserStore();
-//await userStore.fetch();
+userStore.fetch().then(() => {
+    theme.value = userStore.settings.theme;
+});
 </script>
